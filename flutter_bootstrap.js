e81122d5e1add9eb0a1e159a31c0d3eab32db3b0
RathikumarJanushan <janushanr17@gmail.com>
2024-12-27 18:40:54 +0100

    Deploy 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4227553551"
+    serviceWorkerVersion: "1169351947"
   }
 });
